eefed4ffcbf69ee2a178c107b65444dbff947f45
NikolasPastier <xpastier@uniag.sk>
2026-03-21 13:03:57 +0100

    fix: disable turbopack, add standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+  output: 'standalone',
+}
 
-export default nextConfig;
+export default nextConfig
